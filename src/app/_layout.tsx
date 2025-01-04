7d3f13d83dd3f081afca625444d8ad2a1abba101
arafay493 <arafay493@gmail.com>
2025-01-04 17:03:02 +0500

    Updated Folder Structure.

---
+++ src/app/_layout.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { Stack } from 'expo-router'
+
+const RootNavigation = () => {
+  return (
+    <Stack>
+      <Text>RootNavigation</Text>
+    </Stack>
+  )
+}
+
+export default RootNavigation
