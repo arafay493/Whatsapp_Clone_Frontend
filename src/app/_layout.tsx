e3ea8a62a3853a84333fdc601eb40d2222d4768d
arafay493 <arafay493@gmail.com>
2025-01-12 17:11:17 +0500

    Splash Screen work done.

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,8 +1,15 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Stack } from 'expo-router'
-
+import * as SplashScreen from "expo-splash-screen";
+SplashScreen.preventAutoHideAsync();
 const RootNavigation = () => {
+  useEffect(() => {
+    setTimeout(() => {
+      SplashScreen.hideAsync();
+      // perform any necessary tasks here.
+    }, 2000);
+  }, [])
   return (
     <Stack>
       <Stack.Screen name = "index"/>
